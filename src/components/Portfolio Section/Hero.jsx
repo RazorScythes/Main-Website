eb8220838798d18497970f4282b26ac000ff1991
RazorScythes <unknownidentity257@gmail.com>
2023-03-21 11:02:01 +0800

    change font size

--- src/components/Portfolio Section/Hero.jsx
+++ src/components/Portfolio Section/Hero.jsx
@@ -16,14 +16,14 @@ const Hero = () => {
                     <div className="container mx-auto file:lg:px-8 relative px-0">
                         <div className="lg:flex md:flex items-center justify-evenly">
                             <div className="lg:w-3/5 md:w-3/5 w-full sm:px-4">
-                                <h1 style={{lineHeight: "1.2em"}} className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-bold tracking-tighter text-white mb-4 capitalize">
+                                <h1 style={{lineHeight: "1.2em"}} className="text-5xl sm:text-5xl md:text-6xl lg:text-6xl font-bold tracking-tighter text-white mb-4 capitalize">
                                     <span style={{color: "#CD3242"}}>Hello I'm,</span> <br/>James Arvie Maderas
                                 </h1>
                                 <h2 className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-semibold text-white mb-8 tracking-tighter">
                                     Web Developer
                                 </h2>
                                 <p className="text-white text-lg sm:text-xl md:text-lg leading-relaxed mb-4">
-                                Explore the latest games, consoles, and technologies, along with personal stories, insights, and experiences on this website. From reviews and walkthroughs to blog posts and videos, I offer a diverse range of content that is sure to keep you entertained and engaged.
+                                    Explore the latest games, consoles, and technologies, along with personal stories, insights, and experiences on this website. From reviews and walkthroughs to blog posts and videos, I offer a diverse range of content that is sure to keep you entertained and engaged.
                                 </p>
                                 <button className="bg-gray-100 hover:bg-transparent hover:text-gray-100 text-gray-800 font-semibold my-8 py-2 px-8 border border-gray-100 rounded transition-colors duration-300 ease-in-out" onClick={() => loginWithRedirect()}>
                                     Hire Me!
