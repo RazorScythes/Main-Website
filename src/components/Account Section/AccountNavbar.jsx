b352a7361427e868ff2c5af494d949f4b2016c97
RazorScythes <unknownidentity257@gmail.com>
2023-03-24 23:50:20 +0800

    added: user dashboard

--- src/components/Account Section/AccountNavbar.jsx
+++ src/components/Account Section/AccountNavbar.jsx
@@ -97,7 +97,7 @@ const AccountNavbar = ({ path }) => {
                         className={`cursor-pointer text-white hover:text-blue-200 mb-4`}
                       >
                         <FontAwesomeIcon icon={faUser} className="mr-2" />
-                        <a href={`${path}/${user.result.username}`}>Account</a>
+                        <a href={`${path}/account`}>Account</a>
                       </li>
                       <li
                         className={`cursor-pointer text-white hover:text-blue-200 mb-4`}
@@ -184,7 +184,7 @@ const AccountNavbar = ({ path }) => {
                         className={`cursor-pointer text-white hover:text-blue-200 mb-4`}
                       >
                         <FontAwesomeIcon icon={faUser} className="mr-2" />
-                        <a href={`${path}/${user.result.username}`}>Account</a>
+                        <a href={`${path}/account`}>Account</a>
                       </li>
                       <li
                         className={`cursor-pointer text-white hover:text-blue-200 mb-4`}
