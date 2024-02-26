776abd2b59f5a66764ee06b6700423a200e48c35
James Arvie Maderas <unknownidentity257@gmail.com>
2024-02-27 00:41:28 +0800

    projects

--- src/components/Pages/ProjectsSingle.jsx
+++ src/components/Pages/ProjectsSingle.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styles from "../../style";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSearchParams, useParams } from "react-router-dom";
-import { faArrowLeft, faArrowRight, faChevronLeft, faChevronRight, faExternalLink, faFile, faHome, faHomeAlt, faHomeLg, faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faArrowRightRotate, faChevronLeft, faChevronRight, faExternalLink, faFile, faHome, faHomeAlt, faHomeLg, faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { getProjectByID, clearAlert } from "../../actions/project";
 import { useDispatch, useSelector } from 'react-redux'
@@ -102,6 +102,10 @@ const ProjectsSingle = ({ user }) => {
         }).format(dateObj);
 
         return formattedDate
+    }
+
+    const submitComment = () => {
+
     }
 
     return (
@@ -387,6 +391,53 @@ const ProjectsSingle = ({ user }) => {
                                                     )
                                                 })
                                             }
+                                            <div className='flex gap-2'>
+                                                {
+                                                    projectData.project?.tags.length > 0 &&
+                                                    projectData.project?.tags.map((item, index) => {
+                                                        return (
+                                                            <span key={index} className='cursor-pointer transition-all p-4 py-3 text-sm rounded-lg bg-[#131C31] border border-solid border-[#222F43] text-gray-100 hover:text-[#0DBFDC]'>
+                                                                #{item}
+                                                            </span>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                            <h2 className='text-3xl font-semibold my-4 mt-8 text-[#B9E0F2]'>Leave a comment</h2>
+                                            <textarea
+                                                value=""
+                                                // onChange={(e) => setComment(e.target.value)}
+                                                name="message"
+                                                id="message"
+                                                cols="30"
+                                                rows="8"
+                                                placeholder="Write a comment"
+                                                className="w-full p-4 text-sm rounded-lg mt-2 outline-0 transition-all focus:border-gray-600 bg-[#131C31] border border-solid border-[#222F43] text-gray-100 focus:ring-gray-700"
+                                            >
+                                            </textarea>
+                                            <button onClick={submitComment} className="text-sm float-right bg-[#3a8894] hover:bg-transparent hover:bg-[#131C31] text-gray-100 py-2 px-4 border border-[#222F43] rounded transition-colors duration-300 ease-in-out">
+                                                {
+                                                    // !submitted ?
+                                                    (
+                                                        <>
+                                                            Post Comment
+                                                        </>
+                                                    )
+                                                    // :
+                                                    // (
+                                                    //     <div className='flex flex-row justify-center items-center px-4'>
+                                                    //         <div role="status">
+                                                    //             <svg aria-hidden="true" class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    //                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                                    //                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                                    //             </svg>
+                                                    //             <span class="sr-only">Loading...</span>
+                                                    //         </div>
+                                                    //         Commenting
+                                                    //     </div>
+                                                    // )
+                                                }
+                                            </button>
                                         </div>
                                         <div>
                                             HELLO
@@ -439,7 +490,7 @@ const ProjectsSingle = ({ user }) => {
                                     </div> */}
                                 </div>
                                 <div>
-                                    <p>Hi</p>
+                                    {/* <p>Hi</p> */}
                                 </div>
                             </div>
                         </div>
